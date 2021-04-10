e3271d83de2ccb65ac04d26c8a2213ae14e0f064
Chloe Tan <chloe.tan95@gmail.com>
2021-04-10 15:30:08 -0400

    Change IP address for title and reviews services

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App as Title } from '../../chloe-fec-title-service/client/src/index.jsx';
 const container = document.getElementById('root');
 
 console.log('Title: ', Title);
@@ -11,13 +10,10 @@ class Proxy extends React.Component {
   }
 
   render () {
-    console.log('hi');
     return (
-      <div>
-        Hi?
-      </div>
+      <h1>AirBnB</h1>
     );
   }
 }
 
-ReactDOM.render(<h1>hello</h1>, document.getElementById('root'));
+ReactDOM.render(<Proxy />, document.getElementById('root'));
